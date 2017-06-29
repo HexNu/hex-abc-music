eb297e11961f7ac7b07072a495eca8465dc88168
hl <hl@NL394>
2017-06-29 10:41:41 +0200

    added utility method

--- hex-abc-music/abc-music-core/src/main/java/abc/music/core/util/TextUtil.java
+++ hex-abc-music/abc-music-core/src/main/java/abc/music/core/util/TextUtil.java
@@ -13,6 +13,14 @@ public class TextUtil {
         this.str = str;
     }
 
+    public String removeTextBetweenChars(char... chars) {
+        String result = str;
+        for (char c : chars) {
+            result = result.replaceAll(c + ".*?" + c, "");
+        }
+        return result;
+    }
+
     public int nextIndexOf(char c, int start) {
         for (int i = start; i < str.length(); i++) {
             if (str.charAt(i) == c) {
@@ -58,8 +66,23 @@ public class TextUtil {
         }
         return result;
     }
-    
+
     public String capitalizeFirst() {
         return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        String notes = "DF AB/c/ d2 | ce/c/ (dA) AF | (G/A/)G/F/ (EB) A>F | GF/E/ (GF) F>F | (E/D/)C/B,/ A,C/E/ AG |";
+        int i = new TextUtil(notes.replaceAll("\\s", "")).nextIndexOf('|', 20);
+        System.out.println(notes.replaceAll("\\s", "").substring(0, i + 1));
+        notes = "G d B3/ | c B/A/ B3/ | c/B/ A G/A/B/ | A G G F/ | E G/F/ D3/ | ";
+        i = new TextUtil(notes.replaceAll("\\s", "")).nextIndexOf('|', 20);
+        System.out.println(notes.replaceAll("\\s", "").substring(0, i + 1));
+        notes = "\"G\"G A/ G : \"D\"F/E/F/ \"D\"G !pizz.!D | \"C\"E D/ C : C/B,/C/ \"D\"D2 | \"G\"G A/ G : \"D\"F/E/F/ \"G\"G B | ";
+        notes = new TextUtil(notes.replaceAll("\\s", "")).removeTextBetweenChars('!', '"');
+        i = new TextUtil(notes).nextIndexOf('|', 20);
+//        i = new TextUtil(notes.replaceAll("\\s", "").removeTextBetweenChars('!', '"')).nextIndexOf('|', 20);
+//        System.out.println(new TextUtil(notes.replaceAll("\\s", "")).removeTextBetweenChars('!', '"'));
+        System.out.println(notes.substring(0, i + 1));
     }
 }
