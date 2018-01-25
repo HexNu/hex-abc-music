b21b7ed6be1a21a122477d7c6d04f6850763e082
hl <hl@NL394>
2018-01-25 09:49:28 +0100

    Added javafx module

---
+++ hex-abc-music/HexAbcMusicApp/src/main/java/abc/music/app/EditorApp.java
@@ -0,0 +1,42 @@
+package abc.music.app;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import nu.hex.abc.music.service.Service;
+
+/**
+ * Created 2018-jan-24
+ *
+ * @author hl
+ */
+public class EditorApp extends Application {
+
+    private static final String APP_LABEL = "Hex ABC Music Notes Editor";
+    private Service service;
+    private Stage stage;
+    private MenuBar menuBar;
+    private BorderPane root;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
+        initUI();
+    }
+
+    private void initUI() {
+        setupScene(stage);
+        stage.show();
+    }
+
+    private void setupScene(Stage currentStage) {
+        root = new BorderPane();
+        Scene scene = new Scene(root, 1900, 1100);
+        currentStage.setTitle(APP_LABEL);
+        currentStage.setScene(scene);
+        scene.getStylesheets().add("/styles/styles.css");
+    }
+
+}
